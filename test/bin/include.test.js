7e8d6ad91ec05fe54d807caf2cb48d85b058cd3e
Jan-André Zinser <zinserjan@gmail.com>
2016-04-01 23:34:01 +0200

    add tests for --include

---
+++ test/bin/include.test.js
@@ -0,0 +1,95 @@
+/* eslint-env node, mocha */
+/* eslint-disable func-names, prefer-arrow-callback, no-loop-func, max-len */
+
+import path from 'path';
+import { assert } from 'chai';
+import { exec } from 'child_process';
+
+const binPath = path.relative(process.cwd(), path.join('bin', '_mocha'));
+
+const fixtureDir = path.relative(process.cwd(), path.join(__dirname, 'fixture'));
+const helperDir = path.join(fixtureDir, 'include', 'helper');
+const testDir = path.join(fixtureDir, 'include', 'test');
+
+
+const helper1 = `${path.join(helperDir, 'test-helper.js')}`;
+const helper2 = `${path.join(helperDir, 'test-helper-2.js')}`;
+
+
+function testInclude(entry, include, cb) {
+  exec(`node ${binPath} \"${entry}\" --include ${include}`, cb);
+}
+
+function testSingleInclude(entry, done) {
+  return testInclude(entry, helper1, (err, stdout) => {
+    assert.isNull(err);
+    assert.include(stdout, 'first --include works');
+    done();
+  });
+}
+
+function testMultiInclude(entry, done) {
+  return testInclude(entry, `${helper1} ${helper2}`, (err, stdout) => {
+    assert.isNull(err);
+    assert.include(stdout, 'first --include works');
+    assert.include(stdout, 'second --include works');
+    done();
+  });
+}
+
+describe('cli --include', function () {
+  context('file as entry', function () {
+    beforeEach(function () {
+      this.entry = path.join(testDir, 'dependent-test.js');
+    });
+
+    it('single --include', function (done) {
+      testSingleInclude(this.entry, done);
+    });
+
+    it('multiple --include', function (done) {
+      testMultiInclude(this.entry, done);
+    });
+  });
+
+  context('dir as entry', function () {
+    beforeEach(function () {
+      this.entry = testDir;
+    });
+
+    it('single --include', function (done) {
+      testSingleInclude(this.entry, done);
+    });
+
+    it('multiple --include', function (done) {
+      testMultiInclude(this.entry, done);
+    });
+  });
+
+  context('glob as entry', function () {
+    beforeEach(function () {
+      this.entry = path.join(testDir, '*.js');
+    });
+
+    it('single --include', function (done) {
+      testSingleInclude(this.entry, done);
+    });
+
+    it('multiple --include', function (done) {
+      testMultiInclude(this.entry, done);
+    });
+  });
+
+  context('include types', function () {
+    it('include project file', function (done) {
+      testSingleInclude(testDir, done);
+    });
+
+    it('include node_module', function (done) {
+      testInclude(path.join(testDir, 'test.js'), 'chai', (err) => {
+        assert.isNull(err);
+        done();
+      });
+    });
+  });
+});
