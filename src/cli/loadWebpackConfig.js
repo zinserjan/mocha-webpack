523dd23a4ab470eacf19e5ca938dfef7a3cce19f
Dmitriy Pushkov <ezze@ezze.org>
2016-10-08 00:18:43 +0300

    ESLint errors are fixed.

--- src/cli/loadWebpackConfig.js
+++ src/cli/loadWebpackConfig.js
@@ -1,13 +1,21 @@
 import path from 'path';
 import interpret from 'interpret';
 
-const extensions = Object.keys(interpret.extensions).sort(function(a, b) {
-  return a === '.js' ? -1 : b === '.js' ? 1 : a.length - b.length;
-});
+function sortExtensions(ext1, ext2) {
+  if (ext1 === '.js') {
+    return -1;
+  }
+  if (ext2 === '.js') {
+    return 1;
+  }
+  return ext1.length - ext2.length;
+}
+
+const extensions = Object.keys(interpret.extensions).sort(sortExtensions);
 
 function getConfigExtension(configPath) {
   for (let i = extensions.length - 1; i >= 0; i--) {
-    let extension = extensions[i];
+    const extension = extensions[i];
     if (configPath.indexOf(extension, configPath.length - extension.length) > -1) {
       return extension;
     }
@@ -20,16 +28,17 @@ function registerCompiler(moduleDescriptor) {
     return;
   }
 
-  if (typeof moduleDescriptor === "string") {
-    require(moduleDescriptor);
+  if (typeof moduleDescriptor === 'string') {
+    require(moduleDescriptor); // eslint-disable-line global-require
   } else if (!Array.isArray(moduleDescriptor)) {
-    moduleDescriptor.register(require(moduleDescriptor.module));
+    const module = require(moduleDescriptor.module); // eslint-disable-line global-require
+    moduleDescriptor.register(module);
   } else {
     for (let i = 0; i < moduleDescriptor.length; i++) {
       try {
         registerCompiler(moduleDescriptor[i]);
         break;
-      } catch(e) {
+      } catch (e) {
         // do nothing
       }
     }
@@ -45,7 +54,7 @@ export default function loadWebpackConfig(webpackConfig) {
   const webpackConfigExtension = getConfigExtension(webpackConfigPath);
 
   registerCompiler(interpret.extensions[webpackConfigExtension]);
-  webpackConfig = require(webpackConfigPath);
+  const config = require(webpackConfigPath); // eslint-disable-line global-require
 
-  return webpackConfig.default || webpackConfig;
+  return config.default || config;
 }
