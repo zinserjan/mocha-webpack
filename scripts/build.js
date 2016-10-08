40587407bf538484f4faa926c604d17834536df1
Jan-André Zinser <zinserjan@gmail.com>
2016-10-08 23:32:25 +0200

    replace custom release scripts in favour of np (#55)
    * replace custom release scripts in favour of np
    
    * lint only on node versions > 4

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-var execSync = require('child_process').execSync;
-
-function exec(command) {
-  execSync(command, { stdio: [0, 1, 2] });
-}
-
-exec('npm run lint');
-exec('npm run test');
-exec('npm run build');
