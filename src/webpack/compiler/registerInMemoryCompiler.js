4c4d384c4ac7d0d42a5226f774b56f125129bd30
Jan-André Zinser <zinserjan@gmail.com>
2016-12-13 20:15:50 +0100

    support code-splitting like require.ensure

--- src/webpack/compiler/registerInMemoryCompiler.js
+++ src/webpack/compiler/registerInMemoryCompiler.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import sourceMapSupport from 'source-map-support';
 import MemoryFileSystem from 'memory-fs';
 import registerRequireHook from '../util/registerRequireHook';
@@ -16,8 +17,23 @@ export default function registerInMemoryCompiler(compiler: Compiler): void {
     }
   };
 
+  const resolveFile = (filePath, requireCaller) => {
+    // try to read file from memory-fs as it is
+    let code = readFile(filePath);
+
+    if (code === null && requireCaller != null) {
+      const { filename } = requireCaller;
+      if (filename != null) {
+        // if that didn't work, resolve the file relative to it's parent
+        const resolvedPath = path.resolve(path.dirname(filename), filePath);
+        code = readFile(resolvedPath);
+      }
+    }
+    return code;
+  };
+
   // install require hook to be able to require webpack bundles from memory
-  registerRequireHook('.js', readFile);
+  registerRequireHook('.js', resolveFile);
 
   // install source map support to read source map from memory
   sourceMapSupport.install({
---
+++ test/integration/cli/fixture/code-splitting/test/lazy-load-entry-static.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+var assert = require('assert');
+var lazyLoadEntry = require('../src/lazy-load-entry-static');
+
+describe('lazy-load-entry-static', function () {
+  it('runs test', function (cb) {
+    lazyLoadEntry(cb);
+  });
+});
