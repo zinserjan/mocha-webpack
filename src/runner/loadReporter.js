e0960280df3bb280c02769f3417fe787d4cddf7d
Jan-André Zinser <zinserjan@gmail.com>
2016-12-29 19:48:09 +0100

    support custom reporter for mocha

---
+++ src/runner/loadReporter.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import { reporters } from 'mocha';
+
+export default function loadReporter(reporter: string | () => void, cwd: string) {
+  // if reporter is already loaded, just return it
+  if (typeof reporter === 'function') {
+    return reporter;
+  }
+
+  // try to load built-in reporter like 'spec'
+  if (typeof reporters[reporter] !== 'undefined') {
+    return reporters[reporter];
+  }
+
+  let loadedReporter = null;
+  try {
+    // try to load reporter from node_modules
+    loadedReporter = require(reporter); // eslint-disable-line global-require
+  } catch (e) {
+    // try to load reporter from cwd
+    loadedReporter = require(path.resolve(cwd, reporter)); // eslint-disable-line global-require
+  }
+  return loadedReporter;
+}
---
+++ test/integration/cli/fixture/simple/simple.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+var assert = require('assert');
+
+describe('simple test', function () {
+  it('it works', function () {
+    assert.ok(true);
+  });
+});
