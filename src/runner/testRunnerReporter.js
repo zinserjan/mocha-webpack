d87ec532c0f7a2aa3b6ccdf5c968b85f4b80b42f
Jan-André Zinser <zinserjan@gmail.com>
2018-03-19 22:02:02 +0100

    Update the string that clears the console

--- src/runner/testRunnerReporter.js
+++ src/runner/testRunnerReporter.js
@@ -58,7 +58,7 @@ class Reporter {
 
   clearConsole() {
     if (this.interactive) {
-      process.stdout.write(process.platform === 'win32' ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H');
+      process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H');
     }
   }
 
