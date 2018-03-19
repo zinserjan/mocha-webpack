263c17caa57a2d51bebfa7cdfec8f1bc19275e31
Jan-André Zinser <zinserjan@gmail.com>
2018-03-19 21:54:27 +0100

    make eslint happy

--- test/unit/util/registerRequireHook.test.js
+++ test/unit/util/registerRequireHook.test.js
@@ -7,34 +7,34 @@ import fakemodule from './fixture/fakeModule';
 const fakeModulePath = require.resolve('./fixture/fakeModule');
 
 describe('registerRequireHook', function () {
-
   beforeEach(function () {
     delete require.cache[fakeModulePath];
   });
 
   it('requires from disk', function () {
-    const result = require(fakeModulePath);
+    const result = require(fakeModulePath); // eslint-disable-line global-require
     assert.equal(result, fakemodule);
   });
 
   it('requires from memory', function () {
     const dispose = registerRequireHook('.js', (filePath, requireCaller) => {
       if (filePath === fakeModulePath) {
+        const { filename } = requireCaller;
+        assert.equal(filename, __filename);
         return { path: filePath, source: 'module.exports = "from-memory";' };
       }
       return { path: null, source: null };
     });
 
     // module should be read from memory
-    const resultMemory = require(fakeModulePath);
+    const resultMemory = require(fakeModulePath); // eslint-disable-line global-require
     assert.notEqual(resultMemory, fakemodule);
 
     // unregister memory require handler
     dispose();
 
     // module should be read from disk
-    const resultDisk = require(fakeModulePath);
+    const resultDisk = require(fakeModulePath); // eslint-disable-line global-require
     assert.equal(resultDisk, fakemodule);
   });
-
 });
