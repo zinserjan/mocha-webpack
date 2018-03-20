f7e032e00259fc747f24e9f0135a5cfc5a254b44
Jan-André Zinser <zinserjan@gmail.com>
2018-03-20 22:44:27 +0100

    fix eslint after rebase

--- test/unit/util/registerRequireHook.test.js
+++ test/unit/util/registerRequireHook.test.js
@@ -1,9 +1,9 @@
 /* eslint-env node, mocha */
-/* eslint-disable func-names, prefer-arrow-callback, no-loop-func, max-len */
+/* eslint-disable func-names, prefer-arrow-callback */
 import { assert } from 'chai';
 import registerRequireHook from '../../../src/util/registerRequireHook';
-
 import fakemodule from './fixture/fakeModule';
+
 const fakeModulePath = require.resolve('./fixture/fakeModule');
 
 describe('registerRequireHook', function () {
@@ -12,7 +12,7 @@ describe('registerRequireHook', function () {
   });
 
   it('requires from disk', function () {
-    const result = require(fakeModulePath); // eslint-disable-line global-require
+    const result = require(fakeModulePath); // eslint-disable-line
     assert.equal(result, fakemodule);
   });
 
@@ -27,14 +27,14 @@ describe('registerRequireHook', function () {
     });
 
     // module should be read from memory
-    const resultMemory = require(fakeModulePath); // eslint-disable-line global-require
+    const resultMemory = require(fakeModulePath); // eslint-disable-line
     assert.notEqual(resultMemory, fakemodule);
 
     // unregister memory require handler
     dispose();
 
     // module should be read from disk
-    const resultDisk = require(fakeModulePath); // eslint-disable-line global-require
+    const resultDisk = require(fakeModulePath); // eslint-disable-line
     assert.equal(resultDisk, fakemodule);
   });
 });
