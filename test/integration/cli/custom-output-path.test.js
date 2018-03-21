36614fde2c7b452192c336e3364c71fd3de97cf4
Jan-André Zinser <zinserjan@gmail.com>
2018-03-21 14:52:33 +0100

    spelling

--- test/integration/cli/custom-output-path.test.js
+++ test/integration/cli/custom-output-path.test.js
@@ -1,6 +1,5 @@
 /* eslint-env node, mocha */
 /* eslint-disable func-names, prefer-arrow-callback, max-len */
-
 import { assert } from 'chai';
 import path from 'path';
 import fs from 'fs';
@@ -12,28 +11,26 @@ const fixtureDir = path.relative(process.cwd(), path.join(__dirname, 'fixture'))
 const binPath = path.relative(process.cwd(), path.join('bin', '_mocha'));
 
 describe('custom output path', function () {
-  context('uses custom outpath path', function () {
-    before(function () {
-      this.passingTest = normalizePath(path.join(fixtureDir, 'custom-output-path/test/test.js'));
-      this.webpackConfigPath = normalizePath(path.join(fixtureDir, 'custom-output-path/webpack.config-test.js'));
-      this.webpackConfig = require('./fixture/custom-output-path/webpack.config-test.js'); // eslint-disable-line global-require
-    });
+  before(function () {
+    this.passingTest = normalizePath(path.join(fixtureDir, 'custom-output-path/test/test.js'));
+    this.webpackConfigPath = normalizePath(path.join(fixtureDir, 'custom-output-path/webpack.config-test.js'));
+    this.webpackConfig = require('./fixture/custom-output-path/webpack.config-test.js'); // eslint-disable-line global-require
+  });
 
-    beforeEach(function () {
-      return del(this.webpackConfig.output.path);
-    });
+  beforeEach(function () {
+    return del(this.webpackConfig.output.path);
+  });
 
-    it('runs successful test', function (done) {
-      exec(`node ${binPath}  --webpack-config "${this.webpackConfigPath}" "${this.passingTest}"`, (err, output) => {
-        assert.isNull(err);
-        assert.include(output, '1 passing');
-        assert.isTrue(fs.existsSync(path.join(this.webpackConfig.output.path, this.webpackConfig.output.filename)));
-        done();
-      });
+  it('runs test successfully', function (done) {
+    exec(`node ${binPath}  --webpack-config "${this.webpackConfigPath}" "${this.passingTest}"`, (err, output) => {
+      assert.isNull(err);
+      assert.include(output, '1 passing');
+      assert.isTrue(fs.existsSync(path.join(this.webpackConfig.output.path, this.webpackConfig.output.filename)));
+      done();
     });
+  });
 
-    afterEach(function () {
-      return del(this.webpackConfig.output.path);
-    });
+  afterEach(function () {
+    return del(this.webpackConfig.output.path);
   });
 });
--- test/integration/cli/webworker.test.js
+++ test/integration/cli/webworker.test.js
@@ -1,6 +1,5 @@
 /* eslint-env node, mocha */
 /* eslint-disable func-names, prefer-arrow-callback */
-
 import { assert } from 'chai';
 import path from 'path';
 import normalizePath from 'normalize-path';
@@ -11,17 +10,15 @@ const fixtureDir = path.relative(process.cwd(), path.join(__dirname, 'fixture'))
 const binPath = path.relative(process.cwd(), path.join('bin', '_mocha'));
 
 describe('webworker', function () {
-  context('runs tests successfull', function () {
-    before(function () {
-      this.passingTest = normalizePath(path.join(fixtureDir, 'webworker/test/worker.js'));
-      this.webpackConfig = normalizePath(path.join(fixtureDir, 'webworker/webpack.config-test.js'));
-    });
-    it('runs successful test', function (done) {
-      exec(`node ${binPath}  --webpack-config "${this.webpackConfig}" "${this.passingTest}"`, (err, output) => {
-        assert.isNull(err);
-        assert.include(output, '1 passing');
-        done();
-      });
+  before(function () {
+    this.passingTest = normalizePath(path.join(fixtureDir, 'webworker/test/worker.js'));
+    this.webpackConfig = normalizePath(path.join(fixtureDir, 'webworker/webpack.config-test.js'));
+  });
+  it('runs test successfully', function (done) {
+    exec(`node ${binPath}  --webpack-config "${this.webpackConfig}" "${this.passingTest}"`, (err, output) => {
+      assert.isNull(err);
+      assert.include(output, '1 passing');
+      done();
     });
   });
 });
